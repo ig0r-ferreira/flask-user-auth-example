37cfd70e95790ffec0eab1b63653f8c94eb9f718
Igor Ferreira <iferreira.n01@gmail.com>
2023-05-16 10:59:43 -0300

    Read the SECRET KEY from the .env file

--- flask_user_auth/config.py
+++ flask_user_auth/config.py
@@ -1,13 +1,16 @@
 from pathlib import Path
 
+from dotenv import load_dotenv
 from flask import Flask
 
 
 def init_app(app: Flask) -> None:
+    load_dotenv()
+    app.config.from_prefixed_env()
+
     instance_folder = Path(app.instance_path)
     instance_folder.mkdir(exist_ok=True)
     database_uri = f'sqlite:///{instance_folder / "users.db"}'
 
-    app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
     app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
