4b5caea84585489d05f9a6acde4a5a3727d0d8b9
Igor Ferreira <iferreira.n01@gmail.com>
2023-05-15 21:37:47 -0300

    Add save user to database when registering

--- flask_user_auth/views.py
+++ flask_user_auth/views.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
+
+from flask_user_auth.database import User, db
 
 
 def init_app(app: Flask) -> None:
@@ -6,8 +8,14 @@ def init_app(app: Flask) -> None:
     def home():
         return render_template('index.html')
 
-    @app.route('/register')
+    @app.route('/register', methods=['GET', 'POST'])
     def register():
+        if request.method == 'POST':
+            new_user = User(**request.form.to_dict())
+            db.session.add(new_user)
+            db.session.commit()
+            return render_template('secrets.html', username=new_user.name)
+
         return render_template('register.html')
 
     @app.route('/login')
@@ -16,6 +24,7 @@ def init_app(app: Flask) -> None:
 
     @app.route('/secrets')
     def secrets():
+        print(request.args)
         return render_template('secrets.html')
 
     @app.route('/logout')
