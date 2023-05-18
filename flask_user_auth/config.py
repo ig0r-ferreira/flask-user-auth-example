6ea2e23c667ad1ea308a679de3f17cca7f24d80a
Igor Ferreira <iferreira.n01@gmail.com>
2023-05-17 21:23:08 -0300

    Refactor init_app in config.py

--- flask_user_auth/config.py
+++ flask_user_auth/config.py
@@ -5,18 +5,16 @@ from dotenv import load_dotenv
 from flask import Flask
 
 
-def init_app(app: Flask, config: dict[str, Any]) -> None:
+def init_app(app: Flask, test_config: dict[str, Any]) -> None:
     load_dotenv()
     app.config.from_prefixed_env()
 
-    if config:
-        app.config.from_mapping(config)
+    instance_folder = Path(app.instance_path)
+    instance_folder.mkdir(exist_ok=True)
+    database_uri = f'sqlite:///{instance_folder / "users.db"}'
 
-    if not 'SQLALCHEMY_DATABASE_URI' in app.config:
-        instance_folder = Path(app.instance_path)
-        instance_folder.mkdir(exist_ok=True)
-        database_uri = f'sqlite:///{instance_folder / "users.db"}'
-
-        app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    if test_config:
+        app.config.from_mapping(test_config)
